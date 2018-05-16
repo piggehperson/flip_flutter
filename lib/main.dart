eb2c0d603c0d1b15d73c6f1f2f2ef089c77c2ac9
Peter <peter@glaabs.com>
2018-05-16 19:19:45 -0400

    Fix ink ripples showing behind shaded items instead of on top of them

--- lib/main.dart
+++ lib/main.dart
@@ -555,7 +555,7 @@ class _ListItemState extends State<ListItem> {
             label: const Text("Add a Thing"),
             textColor: Theme.of(context).primaryColor,
             highlightElevation: 0.0,
-            highlightedBorderColor: Colors.transparent,
+            //highlightedBorderColor: Colors.transparent,
             shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(6.0)),
           )
         )
